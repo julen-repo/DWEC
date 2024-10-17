7c30f1ff2c31c2a3ad008843c497da5e437f0f7c
julen-repo <julen.hernandez.omanas@iesjulianmarias.es>
2024-10-17 08:45:45 +0200

    actualizado

--- ajedrez.js
+++ ajedrez.js
@@ -8,34 +8,45 @@ const tablero = [
     ["", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", ""]
 ];
+let figuras = [];
+
 function figura(tipo) {
     this.x = 0;
     this.y = 0;
     this.tipo = tipo;
 }
+
+
 figura.prototype.moverA = function (x, y, array) {
     array[y][x] = (this.tipo[0]).toUpperCase();
 }
+
+
 function colocarPieza(pieza, array) {
-    let randomX = Math.floor(Math.random() * 8);
-    let randomY = Math.floor(Math.random() * 8);
-    if (array[randomY][randomX] == "") {
-        array[randomY][randomX] = (pieza.tipo[0]).toUpperCase();
+    let randomX, randomY;
+    do {
+        randomX = Math.floor(Math.random() * 8);
+        randomY = Math.floor(Math.random() * 8);
+    } while (array[randomY][randomX] !== "");
+    array[randomY][randomX] = (pieza.tipo[0]).toUpperCase();
+}
+
+
+function meter_figuras(veces, tipo) {
+    for (let index = 0; index < veces; index++) {
+        figuras.push(new figura(tipo));
     }
 }
-const alfil = new figura("alfil");
-const reina = new figura("reina");
-const rey = new figura("rey");
-const torre = new figura("torre");
-const caballo = new figura("caballo");
-const peon = new figura("peón");
+meter_figuras(8,"peon");
+meter_figuras(2,"alfil");
+meter_figuras(2,"caballos");
+meter_figuras(2,"torres");
+meter_figuras(1,"reina");
+meter_figuras(1,"rey");
 
-colocarPieza(alfil, tablero);
-colocarPieza(reina, tablero);
-colocarPieza(rey, tablero);
-colocarPieza(torre, tablero);
-colocarPieza(caballo, tablero);
-colocarPieza(peon, tablero);
+for(const c of figuras){
+    colocarPieza(c, tablero);
+}
 
 
 console.table(tablero);
