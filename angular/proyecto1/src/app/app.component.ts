6f971b201d4bed9b515f9edb345540388ab6376a
julen-repo <julen.hernandez.omanas@iesjulianmarias.es>
2025-01-14 10:33:19 +0100

    mete cosas

--- angular/proyecto1/src/app/app.component.ts
+++ angular/proyecto1/src/app/app.component.ts
@@ -8,5 +8,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto1';
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  email = 'rpablo@gmail.com';
+  sitio = 'https://stackoverflow.com/questions/37700536/how-can-i-run-a-command-with-administrator-rights-with-visual-studio-code-termin';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+
+  esActivo() {
+    if (this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+
+  ultimos3Sueldos() {
+    let suma = 0;
+    for (let x = 0; x < this.sueldos.length; x++)
+      suma += this.sueldos[x];
+    return suma;
+  }
+  solicitar(sueldo:number){
+    return this.sueldos[sueldo];
+  }
 }
