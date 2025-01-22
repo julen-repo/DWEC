821952919c6ebdc67cfcfb3426a81cd1490f4d74
julen-repo <julen.hernandez.omanas@iesjulianmarias.es>
2025-01-22 09:04:31 +0100

    angular

--- angular/proyecto4/src/app/cronometro/cronometro.component.ts
+++ angular/proyecto4/src/app/cronometro/cronometro.component.ts
@@ -8,6 +8,10 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class CronometroComponent {
   segundo = 0;
+  rojo = "red";
+  azul = "blue";
+  color = "";
+  crono = document.getElementsByClassName('cronometro');
   @Input() inicio: number = 0;
   @Output() multiplo10 = new EventEmitter<number>();
 
@@ -15,8 +19,13 @@ export class CronometroComponent {
     this.segundo = this.inicio;
     setInterval(() => {
       this.segundo++;
-      if (this.segundo % 10 == 0)
-        this.multiplo10.emit(this.segundo);
+      if (this.segundo % 2 == 0) {
+          const htmlElement = this.crono[0] as HTMLElement;
+          htmlElement.style.backgroundColor = '#FF00FF';
+      } else {
+          const htmlElement = this.crono[0] as HTMLElement;
+          htmlElement.style.backgroundColor = '#FFFF00';
+      }
     }, 1000);
   }
 }
---
+++ angular/proyecto5/src/app/formulario/formulario.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  imports: [FormsModule],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css'
+})
+export class FormularioComponent {
+  nombre = '';
+  apellido = '';
+}
